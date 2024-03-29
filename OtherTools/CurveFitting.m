2efdc0c70820d92d85e83ab8c1931bd3a724653d
Small Luo <1009637405@qq.com>
2024-03-29 14:50:53 +0800

    update

--- OtherTools/CurveFitting.m
+++ OtherTools/CurveFitting.m
@@ -1,16 +1,53 @@
 %% Velocity Curve Fitting
 clear;clc;
-syms t;
-D1 = [0.0000; 0.2500; 0.5000; 0.7500; 1.0000];
-Y1 = [0.2145; 0.1526; 0.1219; 0.0932; 0.0626];
-D2 = [0.0000; 0.2500; 0.5000; 0.7500; 1.0000; 99999.];
-Y2 = [0.2145; 0.1526; 0.1219; 0.0932; 0.0626; 0.0000];
+% =============================================  flapping plate near flaps
+% syms t;
+% D1 = [0.0000; 0.2500; 0.5000; 0.7500; 1.0000];
+% Y1 = [0.2145; 0.1526; 0.1219; 0.0932; 0.0626];
+% D2 = [0.0000; 0.2500; 0.5000; 0.7500; 1.0000; 99999.];
+% Y2 = [0.2145; 0.1526; 0.1219; 0.0932; 0.0626; 0.0000];
+% 
+% f  = fittype('a*exp(k*t)','independent','t','coefficients',{'a','k'}); 
+% 
+% P  = fit(D2,Y2,f,'Lower',[-100,-100],'Upper',[100,100],'StartPoint',[1, -1]);
+% D  = 0:0.1:1;
+% Y  = P(D);
+% plot(D1,Y1,'o')
+% hold on
+% plot(D,Y,'-')
+% ====================================  velocity profile behind a cylinder
 
-f  = fittype('a*exp(k*t)','independent','t','coefficients',{'a','k'}); 
-
-P  = fit(D2,Y2,f,'Lower',[-100,-100],'Upper',[100,100],'StartPoint',[1, -1]);
-D  = 0:0.1:1;
-Y  = P(D);
-plot(D1,Y1,'o')
-hold on
-plot(D,Y,'-')
+filePath = 'G:\OpenFOAM-v2306\oversetMesh\wingMotion\cylinder\VTK\velocityProfile\ProfileX1.00.txt';
+fileData = importdata(filePath);
+y = fileData(:,1);
+u = fileData(:,2);
+fprintf('==============================================================\n')
+fprintf('{')
+for i = 1:size(y,1)-1
+    fprintf('%f,',y(i))
+    if mod(i,10)==0
+        fprintf('\\\n')
+    end
+end
+fprintf('%f',y(end))
+fprintf('}\n')
+fprintf('==============================================================\n')
+fprintf('{')
+for i = 1:size(y,1)-1
+    fprintf('%f,',u(i))
+    if mod(i,10)==0
+        fprintf('\\\n')
+    end
+end
+fprintf('%f',u(end))
+fprintf('}\n')
+fprintf('==============================================================\n')
+yt = (-2.90:0.01:2.90);
+for i = 1:length(yt)
+    for j = 1:size(y,1)-1
+        if y(j)<=yt(i)&&y(j+1)>=yt(i)
+            ut(i) = u(j)+(u(j+1)-u(j))*(yt(i)-y(j))/(y(j+1)-y(j));
+        end
+    end
+end
+plot(y,u,'r',yt,ut,'b*')
--- OtherTools/NACAWingBoundary.m
+++ OtherTools/NACAWingBoundary.m
@@ -1,7 +1,7 @@
 clear;clc;close all;
 % NACA00xx wing boundary creation
-h     = 0.160;  % NACA00xx, h = 0.xx
-angle = pi/18;  % attack angle (pi)
+h     = 0.120;  % NACA00xx, h = 0.xx
+angle = 0.000;  % attack angle (pi)
 pivot = 0.000;  % pivot point in x axis
 x = [0.000:0.001:0.049 0.05:0.005:1.00];
 for i = 1:length(x)
@@ -22,7 +22,7 @@ center = zeros(1,3);
 for i = 1:length(x)-1
     center(1) = center(1) + (x(i+1)+x(i))/2*(x(i+1)-x(i))*(y(i+1)+y(i))/area;
 end
-ncenter(1) =  (center(1)-pivot)*cos(angle)+center(2)*sin(angle);
+ncenter(1) =  (center(1)-pivot)*cos(angle)+center(2)*sin(angle)+pivot;
 ncenter(2) = -(center(1)-pivot)*sin(angle)+center(2)*cos(angle);
 fprintf('centerOfMass :(%f %f)\n', ncenter)
 %moment of inertia
@@ -42,16 +42,16 @@ for i = 1:length(xn)-1
 end
 fprintf('momentOfZAxis: %f\n',momentZ)
 %% plot
-nx1 =  (x-pivot)*cos(angle)+y*sin(angle);
+nx1 =  (x-pivot)*cos(angle)+y*sin(angle)+pivot;
 ny1 = -(x-pivot)*sin(angle)+y*cos(angle);
-nx2 =  (x-pivot)*cos(angle)-y*sin(angle);
+nx2 =  (x-pivot)*cos(angle)-y*sin(angle)+pivot;
 ny2 = -(x-pivot)*sin(angle)-y*cos(angle);
 
-plot(nx1, ny1,'r', nx2, ny2,'b','LineWidth',1.2)
-xlim([0-pivot 1-pivot]); ylim([-0.5 0.5]);
+plot(nx1, ny1,'r', nx2, ny2,'k','LineWidth',1.2)
+xlim([0 1]); ylim([-0.5 0.5]);
 
 %% output blockMesh parameters
-num = find(x==0.04);
+num = find(x==0.02);
 fprintf('fixed point 08:(%f10 %f10)\n', nx2(num), ny2(num))
 fprintf('fixed point 09:(%f10 %f10)\n', nx2(end), ny2(end))
 fprintf('fixed point 11:(%f10 %f10)\n', nx1(num), ny1(num))
@@ -59,35 +59,44 @@ fprintf('fixed point 12:(%f10 %f10)\n', nx1(end), ny1(end))
 fprintf('============================================\n')
 filename = '.\Scripts\NACAWingBoundary.dat';
 file=fopen(filename,'w');
-fprintf(file,'1============================================\n');
-for i = num-1:-1:1
-    fprintf(file,'        (%f %f $z0)\n',nx2(i),ny2(i));
+%% write for blockmesh
+% fprintf(file,'1============================================\n');
+% for i = num-1:-1:1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z0)\n',nx2(i),ny2(i));
+% end
+% for i = 2:1:num-1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z0)\n',nx1(i),ny1(i));
+% end
+% fprintf(file,'2============================================\n');
+% for i = num+1:length(x)-1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z0)\n',nx2(i),ny2(i));
+% end
+% fprintf(file,'3============================================\n');
+% for i = num+1:length(x)-1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z0)\n',nx1(i),ny1(i));
+% end
+% fprintf(file,'4============================================\n');
+% for i = num-1:-1:1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z1)\n',nx2(i),ny2(i));
+% end
+% for i = 2:1:num-1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z1)\n',nx1(i),ny1(i));
+% end
+% fprintf(file,'5============================================\n');
+% for i = num+1:length(x)-1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z1)\n',nx2(i),ny2(i));
+% end
+% fprintf(file,'6============================================\n');
+% for i = num+1:length(x)-1
+%     fprintf(file,'        (%f ${{%f + $dh}} $z1)\n',nx1(i),ny1(i));
+% end
+%% write for pointwise
+fprintf(file,'%d\n',length(x)*2);
+for i = 1: 1:length(x)
+    fprintf(file,'%f %f -2.50\n',nx1(i),ny1(i));
 end
-for i = 2:1:num-1
-    fprintf(file,'        (%f %f $z0)\n',nx1(i),ny1(i));
-end
-fprintf(file,'2============================================\n');
-for i = num+1:length(x)-1
-    fprintf(file,'        (%f %f $z0)\n',nx2(i),ny2(i));
-end
-fprintf(file,'3============================================\n');
-for i = num+1:length(x)-1
-    fprintf(file,'        (%f %f $z0)\n',nx1(i),ny1(i));
-end
-fprintf(file,'4============================================\n');
-for i = num-1:-1:1
-    fprintf(file,'        (%f %f $z1)\n',nx2(i),ny2(i));
-end
-for i = 2:1:num-1
-    fprintf(file,'        (%f %f $z1)\n',nx1(i),ny1(i));
-end
-fprintf(file,'5============================================\n');
-for i = num+1:length(x)-1
-    fprintf(file,'        (%f %f $z1)\n',nx2(i),ny2(i));
-end
-fprintf(file,'6============================================\n');
-for i = num+1:length(x)-1
-    fprintf(file,'        (%f %f $z1)\n',nx1(i),ny1(i));
+for i = length(x):-1:1
+    fprintf(file,'%f %f -2.50\n',nx2(i),ny2(i));
 end
 fclose all;
 
