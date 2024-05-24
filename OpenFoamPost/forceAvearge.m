b375d0602b49e9331015d0812ce0beb8a109b0fd
Small Luo <xianguangLuo@mail.ustc.edu.cn>
2024-05-24 16:16:27 +0800

    update codes

--- OpenFoamPost/forceAvearge.m
+++ OpenFoamPost/forceAvearge.m
@@ -1,39 +1,46 @@
 clear;clc;close all
 %% Parameters
-fileName  = ['St0.25';'St0.50';'St0.75';'St1.00';'St1.25'];
-filePath  = 'G:\OpenFOAM-v2306\dynamicMesh\infiniteWingMotion\Heaving3D\H0.00\Re2000L1.00A2.00-n\';
+fileName  = ['AoA15Alpha00F1.00-v';'AoA15Alpha02F1.00-v';'AoA15Alpha04F1.00-v';'AoA15Alpha06F1.00-v';'AoA15Alpha08F1.00-v'];
+filePath  = 'G:\OpenFOAM-v2306\dynamicMesh\infiniteMovingWing\CaseAoA15PitchingRe2000\';
 fileNum   = size(fileName,1);
-forceRef  = 0.5 * 1000 * 6;   % F_ref = 0.5 * rho * Sref
+forceRef  = 0.5 * 1000 * 5;   % F_ref = 0.5 * rho * Sref
+beginTime = 5.0;              % the start time for average
 %% Averaging
 meanDrag  = zeros(fileNum,3);
 meanLift  = zeros(fileNum,3);
 for i=1:fileNum
     forceData = importdata([filePath fileName(i,:) '\postProcessing\forcesWing\0\force.dat']).data;
-    forceLen  = floor(size(forceData,1)/2);
     forceTime = forceData(:,1);
+
+    % get last five period
+    for n = 2:size(forceData,1)
+        if(forceTime(n-1)<=beginTime && forceTime(n)>beginTime)
+            beginLen = n - 1;
+        end
+    end
     %  total force, pressure force, viscous force
     forceDrag = [forceData(:,2) forceData(:,5) forceData(:,8)]/forceRef;
     forceLift = [forceData(:,3) forceData(:,6) forceData(:,9)]/forceRef; 
-    for j=forceLen:size(forceData,1)-1
+    for j=beginLen:size(forceData,1)-1
         meanDrag(i,:) = meanDrag(i,:) + 0.5*(forceTime(j+1) - forceTime(j-1))*forceDrag(j,:);
         meanLift(i,:) = meanLift(i,:) + 0.5*(forceTime(j+1) - forceTime(j-1))*forceLift(j,:);
     end
-    meanDrag(i,:)  = meanDrag(i,:)/(forceTime(end-1) - forceTime(forceLen));
-    meanLift(i,:)  = meanLift(i,:)/(forceTime(end-1) - forceTime(forceLen));
+    meanDrag(i,:)  = meanDrag(i,:)/(forceTime(end-1) - forceTime(beginLen));
+    meanLift(i,:)  = meanLift(i,:)/(forceTime(end-1) - forceTime(beginLen));
 end
 %% Write Data
 %  drag
-filename = [filePath 'dataPosting\averageDrag.dat'];
+filename = [filePath 'PostProcessing\averageDrag-v.dat'];
 file     = fopen(filename,'w');
-fprintf(file,'VARIABLES="St_c","f_t","f_p","f_v"\n');
+fprintf(file,'VARIABLES="AoA","f_t","f_p","f_v"\n');
 for i=1:fileNum
-    fprintf(file,'%6f    %6f    %6f    %6f\n',str2num(fileName(i,end-3:end)),meanDrag(i,1),meanDrag(i,2),meanDrag(i,3));
+    fprintf(file,'%6f    %6f    %6f    %6f\n',str2num(fileName(i,11:12)),meanDrag(i,1),meanDrag(i,2),meanDrag(i,3));
 end
 %  lift
-filename = [filePath 'dataPosting\averageLift.dat'];
+filename = [filePath 'PostProcessing\averageLift-v.dat'];
 file     = fopen(filename,'w');
-fprintf(file,'VARIABLES="St_c","f_t","f_p","f_v"\n');
+fprintf(file,'VARIABLES="AoA","f_t","f_p","f_v"\n');
 for i=1:fileNum
-    fprintf(file,'%6f    %6f    %6f    %6f\n',str2num(fileName(i,end-3:end)),meanLift(i,1),meanLift(i,2),meanLift(i,3));
+    fprintf(file,'%6f    %6f    %6f    %6f\n',str2num(fileName(i,11:12)),meanLift(i,1),meanLift(i,2),meanLift(i,3));
 end
 fclose all;
